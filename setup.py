a1e8f0be1038d44e7aead04ad387d6f837972053
Inno Mgubhe <1806exe@gmail.com>
2023-09-15 12:45:47 +0300

    restructure packages and manifest and rename package names

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 import re
 from setuptools import setup
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='port-lens',
     packages=['port_lens'],
@@ -11,6 +16,8 @@ setup(
         "console_scripts": ["port-lens=port_lens.main:main"]
     },
     description='A simple port scanner that works.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/1806exe/port-lens',
     author='Inno Mgubhe',
     author_email='1806exe@email.com',
