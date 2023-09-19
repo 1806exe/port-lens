d1753ed11d1aba7ce003cd55c0162ba8be171f5e
Inno Mgubhe <1806exe@gmail.com>
2023-09-19 08:14:55 +0300

    statrt writing tests update test action

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,22 @@
 import pytest
+from io import StringIO
+import sys
+import os
+
+# Import the code you want to test
+from port_lens.main import main  # Replace 'your_code_file' with the actual filename
+
+def test_main(monkeypatch, capsys):
+    args = ['google.com', '20', '30']  # Modify the arguments here
+    monkeypatch.setattr(sys, 'argv', ['script_name'] + args)
+    monkeypatch.setattr(os, 'system', lambda x: None)  # Mock the os.system function
+
+    try:
+        main()
+        captured = capsys.readouterr()
+        assert "Scanning completed" in captured.out  # Check for a completion message
+    except SystemExit as e:
+        assert e.code == 0
+
+if __name__ == "__main__":
+    pytest.main()
